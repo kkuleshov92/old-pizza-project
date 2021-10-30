378c0a4e403442a02cfaa680bc9a452e84fe2e92
Kirill Kuleshov <kkuleshov92@gmail.com>
2021-10-30 19:34:10 +0300

    fix

--- src/containers/ConfigMaker/ConfigMaker.jsx
+++ src/containers/ConfigMaker/ConfigMaker.jsx
@@ -5,6 +5,7 @@ import Radio from "./components/Radio";
 import Checkbox from "./components/Checkbox";
 import {useOrderListContext} from "../../context/OrderList";
 import OrderList from "../OrderList/OrderList";
+import {pizzaAdditionalFields, pizzaRequiredFields} from "./ConfigMakerData";
 
 
 const ConfigMaker = () => {
@@ -15,172 +16,12 @@ const ConfigMaker = () => {
 
   const [showOrder, setShowOrder] = useState(false);
 
-  const pizzaRequiredFields = [
-    {
-      propNameSection: 'Размер',
-      propCode: 'size',
-      varArray: [
-        {
-          propTitle: '30 см',
-          price: 0,
-          id: 1,
-        },
-        {
-          propTitle: '35 см',
-          price: 50,
-          id: 2,
-        }
-      ]
-    },
-    {
-      propNameSection: 'Тесто',
-      propCode: 'dough',
-      varArray: [
-        {
-          propTitle: 'Тонкое',
-          price: 0,
-          id: 1,
-        },
-        {
-          propTitle: 'Пышное',
-          price: 0,
-          id: 2,
-        }
-      ]
-    },
-    {
-      propNameSection: 'Соус',
-      propCode: 'sauce',
-      varArray: [
-        {
-          propTitle: 'томатный',
-          price: 0,
-          id: 1,
-        },
-        {
-          propTitle: 'белый',
-          price: 0,
-          id: 2,
-        },
-        {
-          propTitle: 'острый',
-          price: 0,
-          id: 3,
-        }
-      ]
-    },
-  ]
-
-  const pizzaAdditionalFields = [
-    {
-      propNameSection: 'Добавьте сыр',
-      propCode: 'cheese',
-      varArray: [
-        {
-          id: 1,
-          propTitle: 'Моцарелла',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 2,
-          propTitle: 'Чеддер',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 3,
-          propTitle: 'Дор Блю',
-          price: 29,
-          img: '',
-        },
-      ]
-    },
-    {
-      propNameSection: 'Добавьте овощи',
-      propCode: 'vegetables',
-      varArray: [
-        {
-          id: 1,
-          propTitle: 'Томаты',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 2,
-          propTitle: 'Грибы',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 3,
-          propTitle: 'Перец',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 4,
-          propTitle: 'Ананас',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 5,
-          propTitle: 'Оливки',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 6,
-          propTitle: 'Лук',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 7,
-          propTitle: 'Брокколи',
-          price: 29,
-          img: '',
-        },
-      ]
-    },
-    {
-      propNameSection: 'Добавьте мясо',
-      propCode: 'meat',
-      varArray: [
-        {
-          id: 1,
-          propTitle: 'Бекон',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 2,
-          propTitle: 'Пепперони',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 3,
-          propTitle: 'Ветчина',
-          price: 29,
-          img: '',
-        },
-        {
-          id: 4,
-          propTitle: 'Курица',
-          price: 29,
-          img: '',
-        },
-      ]
-    },
-  ]
-
   useEffect(() => {
     const oldPrice = state.price;
     let price = +state.defaultPrice;
 
     for (let key in state.settings) {
+      // eslint-disable-next-line
       state.settings[key].arrProp.forEach(item => {
         price += item.price;
       })
@@ -189,7 +30,7 @@ const ConfigMaker = () => {
     if (oldPrice !== price) {
       calcPriceOrder(price)
     }
-  }, [state]);
+  }, [state, calcPriceOrder]);
 
   const submitHandler = (e) => {
     e.preventDefault();
---
+++ src/containers/ConfigMaker/ConfigMakerData.js
@@ -0,0 +1,160 @@
+export const pizzaRequiredFields = [
+  {
+    propNameSection: 'Размер',
+    propCode: 'size',
+    varArray: [
+      {
+        propTitle: '30 см',
+        price: 0,
+        id: 1,
+      },
+      {
+        propTitle: '35 см',
+        price: 50,
+        id: 2,
+      }
+    ]
+  },
+  {
+    propNameSection: 'Тесто',
+    propCode: 'dough',
+    varArray: [
+      {
+        propTitle: 'Тонкое',
+        price: 0,
+        id: 1,
+      },
+      {
+        propTitle: 'Пышное',
+        price: 0,
+        id: 2,
+      }
+    ]
+  },
+  {
+    propNameSection: 'Соус',
+    propCode: 'sauce',
+    varArray: [
+      {
+        propTitle: 'томатный',
+        price: 0,
+        id: 1,
+      },
+      {
+        propTitle: 'белый',
+        price: 0,
+        id: 2,
+      },
+      {
+        propTitle: 'острый',
+        price: 0,
+        id: 3,
+      }
+    ]
+  },
+]
+
+export const pizzaAdditionalFields = [
+  {
+    propNameSection: 'Добавьте сыр',
+    propCode: 'cheese',
+    varArray: [
+      {
+        id: 1,
+        propTitle: 'Моцарелла',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 2,
+        propTitle: 'Чеддер',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 3,
+        propTitle: 'Дор Блю',
+        price: 29,
+        img: '',
+      },
+    ]
+  },
+  {
+    propNameSection: 'Добавьте овощи',
+    propCode: 'vegetables',
+    varArray: [
+      {
+        id: 1,
+        propTitle: 'Томаты',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 2,
+        propTitle: 'Грибы',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 3,
+        propTitle: 'Перец',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 4,
+        propTitle: 'Ананас',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 5,
+        propTitle: 'Оливки',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 6,
+        propTitle: 'Лук',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 7,
+        propTitle: 'Брокколи',
+        price: 29,
+        img: '',
+      },
+    ]
+  },
+  {
+    propNameSection: 'Добавьте мясо',
+    propCode: 'meat',
+    varArray: [
+      {
+        id: 1,
+        propTitle: 'Бекон',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 2,
+        propTitle: 'Пепперони',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 3,
+        propTitle: 'Ветчина',
+        price: 29,
+        img: '',
+      },
+      {
+        id: 4,
+        propTitle: 'Курица',
+        price: 29,
+        img: '',
+      },
+    ]
+  },
+]
